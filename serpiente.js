2d2e5798f69dc6ae35a2bf01a8b580699a4ce3e4
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-05-12 12:45:39 -0500

    Agregar cuadricula al canvas

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,39 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+// 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+const TAMANIO_CELDA = 25;
 
 
-    
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+// Primera pintura del juego al cargar la página
+dibujarTodo();
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+function dibujarTablero() {
+  ctx.strokeStyle = "white" 
+  ctx.beginPath()
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+  for(let i=0; i<=canvas.width; i+=TAMANIO_CELDA){
+    ctx.moveTo(i, 0)
+    ctx.lineTo(i, canvas.width)
+    ctx.stroke()
+  }
+  for(let i=0; i<=canvas.height; i+=TAMANIO_CELDA){
+    ctx.moveTo(0, i)
+    ctx.lineTo(canvas.height, i)
+    ctx.stroke()
+  }
+}
+
+function limpiarCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero();
+}
 
 
 
