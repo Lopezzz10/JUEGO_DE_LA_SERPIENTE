ddab3bfdc270d168d1f9c960415ca4c2b435cd79
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-05-14 13:56:37 -0500

    Agrega función pintarParte como función independiente

--- serpiente.js
+++ serpiente.js
@@ -1,28 +1,55 @@
 const canvas = document.getElementById("canvasJuego");
 const ctx = canvas.getContext("2d");
 const TAMANIO_CELDA = 25;
-const SERPIENTE = [{x:10,y:10},
-                  {x:10,y:11},
-                  {x:11,y:11},
-                  {x:12,y:11},
-                  {x:13,y:11},
-                  {x:13,y:12},
-                  {x:14,y:12}];
+
+const SERPIENTE = [
+  {x:10, y:10},
+  {x:10, y:11},
+  {x:11, y:11},
+  {x:12, y:11},
+  {x:13, y:11},
+  {x:13, y:12},
+  {x:14, y:12}
+];
+
 dibujarTodo();
 
 function dibujarTablero() {
-  ctx.strokeStyle = "white" 
-  ctx.beginPath()
+  ctx.strokeStyle = "white";
+  ctx.beginPath();
 
-  for(let i=0; i<=canvas.width; i+=TAMANIO_CELDA){
-    ctx.moveTo(i, 0)
-    ctx.lineTo(i, canvas.width)
-    ctx.stroke()
+  for (let i = 0; i <= canvas.width; i += TAMANIO_CELDA) {
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, canvas.height);
+    ctx.stroke();
   }
-  for(let i=0; i<=canvas.height; i+=TAMANIO_CELDA){
-    ctx.moveTo(0, i)
-    ctx.lineTo(canvas.height, i)
-    ctx.stroke()
+
+  for (let i = 0; i <= canvas.height; i += TAMANIO_CELDA) {
+    ctx.moveTo(0, i);
+    ctx.lineTo(canvas.width, i);
+    ctx.stroke();
+  }
+}
+
+function pintarParte(lineaX, lineaY) {
+  const x = lineaX * TAMANIO_CELDA;
+  const y = lineaY * TAMANIO_CELDA;
+
+  ctx.fillRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+  ctx.strokeRect(x, y, TAMANIO_CELDA, TAMANIO_CELDA);
+}
+
+function pintarSerpiente() {
+  for (let i = 0; i < SERPIENTE.length; i++) {
+    if (i === 0) {
+      ctx.fillStyle = "yellow";
+    } else {
+      ctx.fillStyle = "red";
+    }
+    pintarParte(SERPIENTE[i].x, SERPIENTE[i].y);
   }
 }
 
@@ -33,31 +60,5 @@ function limpiarCanvas() {
 function dibujarTodo() {
   limpiarCanvas();
   dibujarTablero();
-  pintarSerpiente()
-}
-
-function pintarSerpiente() {
-  for(let i = 0; i < SERPIENTE.length; i++) {
-    if(i == 0) {
-      ctx.fillStyle = "yellow";
-    } else {
-      ctx.fillStyle = "red";
-    }
-    
-    ctx.fillRect(
-      SERPIENTE[i].x * TAMANIO_CELDA,
-      SERPIENTE[i].y * TAMANIO_CELDA,
-      TAMANIO_CELDA,
-      TAMANIO_CELDA
-    );
-    
-    ctx.strokeStyle = "black"; 
-    ctx.lineWidth = 2;          
-    ctx.strokeRect(
-      SERPIENTE[i].x * TAMANIO_CELDA,
-      SERPIENTE[i].y * TAMANIO_CELDA,
-      TAMANIO_CELDA,
-      TAMANIO_CELDA
-    );
-  }
+  pintarSerpiente();
 }
